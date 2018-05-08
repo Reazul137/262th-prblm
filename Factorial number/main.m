9a28720ba788d477e3f6bd91ae0e20223fe6e3bc
Your Name <reazulislam137@gmail.com>
2018-05-08 20:52:46 +0600

    first commit

--- Factorial number/main.m
+++ Factorial number/main.m
@@ -8,10 +8,43 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+@interface SampleClass:NSObject
+/* method declaration */
+- (int)max:(int)num1;
+
+@end
+
+@implementation SampleClass
+
+/* method returning the max between two numbers */
+- (int)max:(int)num1{
+    /* local variable declaration */
+    int result=1, i;
+    for(i=1; i<=num1; i++)
+    {
+        
+        result = result * i;
     }
+    
+    return result;
+}
+
+@end
+
+int main ()
+{
+    /* local variable definition */
+    int a;
+    int ret;
+    
+    
+    SampleClass *sampleClass = [[SampleClass alloc]init];
+    scanf("%d",&a);
+    
+    /* calling a method to get max value */
+    ret = [sampleClass max:a];
+    
+    NSLog(@"Max value is : %d\n", ret );
+    
     return 0;
 }
